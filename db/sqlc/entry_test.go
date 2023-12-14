03782ffc1e19e5687e2e921bbcab3d9e09ce4d14
Olumide Akinkunmi <mide.akinkunmi@gmail.com>
2023-12-14 16:29:00 +0100

    Lesson 7: Writing Unit Tests for DB CRUD Operations

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,78 @@
+package db
+
+import (
+	"simplebank/util"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateEntry(t *testing.T) {
+	arg := CreateEntryParams{
+		AccountID: 17,
+		Amount:    20,
+	}
+
+	entry, err := testQueries.CreateEntry(ctx, arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.AccountID)
+	require.NotZero(t, entry.Amount)
+}
+
+func createRandomEntry(t *testing.T) Entry {
+	arg := CreateEntryParams{
+		AccountID: util.RandomInt(1, 59),
+		Amount:    util.RandomMoney(),
+	}
+
+	entry, err := testQueries.CreateEntry(ctx, arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.AccountID)
+	require.NotZero(t, entry.Amount)
+
+	return entry
+}
+
+func TestGetEntry(t *testing.T) {
+	entry1 := createRandomEntry(t)
+	entry2, err := testQueries.GetEntry(ctx, entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt.Time, entry2.CreatedAt.Time, time.Second)
+}
+
+func TestListEntries(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomEntry(t)
+	}
+
+	arg := ListEntriesParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	entries, err := testQueries.ListEntries(ctx, arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+	}
+}
---
+++ db/sqlc/main_test.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	ctx := context.Background()
+	conn, err := pgx.Connect(ctx, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+	defer conn.Close(ctx)
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+
+}
